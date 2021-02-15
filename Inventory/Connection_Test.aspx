d124b38e8a28bd8547a992d1f03f9fd303e4a208
Fernando <Fgonzalez1@student.cvtc.edu>
2021-02-15 14:11:56 -0600

    Added files needed for database connection

---
+++ Inventory/Connection_Test.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Connection_Test.aspx.cs" Inherits="Inventory.Connection_Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <br /><br />
+
+        <div>
+
+            <!-- Adding buttons to retrieve the database data -->
+            <asp:Button ID="dashBtn" runat="server" Text="DashBoard" OnClick="Retrieve_Dashboard" /><br /><br />
+            <asp:Button ID="categoriesBtn" runat="server" Text="Categories" OnClick="Retrieve_Categories" />
+
+            <br />
+            <asp:GridView ID="GridView1" runat="server" HorizontalAlign="Center"></asp:GridView>
+        </div>
+
+    </form>
+
+</body>
+</html>
---
+++ Inventory/Connection_Test.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+
+namespace Inventory
+{
+    public partial class Connection_Test : System.Web.UI.Page
+
+        //as the title of the file says, this is for making test to the database to ensure connectivity between the app and
+        //the Database
+    {
+
+        protected void Retrieve_Dashboard(object sender, EventArgs e)
+        {
+            //Connection to the inventory table, and display it on the connection_test page
+            string mycon = "server =localhost; Uid=root; password = root ; persistsecurityinfo = True; database =inventory_test; SslMode = none";
+            MySqlConnection con = new MySqlConnection(mycon);
+            DataTable dt = new DataTable();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("Select * from Inventory", con);
+                con.Open();
+                dt.Load(cmd.ExecuteReader());
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+                con.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void Retrieve_Categories(object sender, EventArgs e)
+        {
+            //connection to the category table, and displays it on the page.
+            string mycon = "server =localhost; Uid=root; password = root ; persistsecurityinfo = True; database =inventory_test; SslMode = none";
+            MySqlConnection con = new MySqlConnection(mycon);
+            DataTable dt = new DataTable();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("Select * from Category", con);
+                con.Open();
+                dt.Load(cmd.ExecuteReader());
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+                con.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+    }
+}
