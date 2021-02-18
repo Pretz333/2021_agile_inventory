0bb80a2dea052c538a87f1f5b54249f715b50f2d
jheinzke <jheinzke1@student.cvtc.edu>
2021-02-18 12:24:28 -0600

    Created the add items function

--- Inventory/Items.aspx.designer.cs
+++ Inventory/Items.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace Inventory
         protected global::System.Web.UI.WebControls.SqlDataSource SQLDSItems;
 
         /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-
-        /// <summary>
         /// gvItems control.
         /// </summary>
         /// <remarks>
