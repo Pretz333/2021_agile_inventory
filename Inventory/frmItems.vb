0d8c4934ad3e52576591f73b26972e2d833b0e9c
Pretz333 <wpretzel@hotmail.com>
2021-03-09 13:28:15 -0600

    Organize code, add the ability to get the CategoryID of the selected Item from the dropdown list

--- Inventory/frmItems.vb
+++ Inventory/frmItems.vb
@@ -5,12 +5,14 @@ Public Class frmItems
     Dim ct As New DataTable
     Dim dataAdapter As SqlDataAdapter
     Dim catAdapter As SqlDataAdapter
-    Dim varRowtext As String
 
     Private Sub frmCategories_Load(sender As Object, e As EventArgs) Handles Me.Load
         LoadTableData(String.Empty)
+        PopulateDropdown()
         dgvItems.Columns.Item(0).AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
         dgvItems.Columns.Item(1).AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        dgvItems.Columns.Item(2).AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        dgvItems.Columns.Item(3).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
     End Sub
 
     'Set up connection to database
@@ -28,48 +30,58 @@ Public Class frmItems
         Return dbConnection
     End Function
 
+    Private Sub PopulateDropdown()
+        Dim dbConnection As SqlConnection = ConnectToDb()
+        dbConnection.Open()
+        Dim selectCat As String = "Select * from Category"
+        catAdapter = New SqlDataAdapter(selectCat, dbConnection)
+        catAdapter.Fill(ct)
+        Categories.DisplayMember = "Description"
+        Categories.ValueMember = "Description"
+        Categories.DataSource = ct
+        dbConnection.Close()
+    End Sub
+
     Public Sub LoadTableData(ByVal searchTerm As String)
         Dim dbConnection As SqlConnection = ConnectToDb()
         dbConnection.Open()
-        Dim selectStatement As String = "SELECT ItemId, Category.Description, Item.Description FROM Item inner join Category on item.CategoryId = Category.categoryId"
-        Dim selectCat As String = "Select * from Category"
+        Dim selectStatement As String = "SELECT ItemId, Item.Description, Category.Description FROM Item INNER JOIN Category on item.CategoryId = Category.CategoryId"
         If searchTerm IsNot String.Empty Then
-            Command.Append(" WHERE item.Description LIKE '%" + searchTerm + "%'")
+            selectStatement.Append(" WHERE item.Description LIKE '%" + searchTerm + "%'")
         End If
         dataAdapter = New SqlDataAdapter(selectStatement, dbConnection)
-        catAdapter = New SqlDataAdapter(selectCat, dbConnection)
 
-        'comboBox information
-        Categories.DisplayMember = "Description"
-        Categories.ValueMember = "Description"
-        Categories.DataSource = catRetrieval()
         dataAdapter.Fill(ds)
         dgvItems.DataSource = ds.Tables(0)
         dbConnection.Close()
     End Sub
 
-    'retrieve the data fir the dropdownlist
-    Private Function catRetrieval() As Object
-        'This gives the full path into the bin/debug folder
-        Dim strPath As String = Application.StartupPath
-        Dim intPathLength As Integer = strPath.Length
+    'Events for the change in dropdownlist
+    Private Sub dgvItems_CellContentClick(ByVal sender As System.Object, ByVal e As DataGridViewEditingControlShowingEventArgs) Handles dgvItems.EditingControlShowing
+        ' Only do this for the dropdown, not the input fields
+        If e.Control.GetType().ToString() = "System.Windows.Forms.DataGridViewComboBoxEditingControl" Then
+            Dim editingComboBox As ComboBox = e.Control
+            AddHandler editingComboBox.SelectedIndexChanged, AddressOf editingComboBox_SelectedIndexChanged
+        End If
+    End Sub
 
-        'This strips off the bin/debug folder to point into your project folder.
-        strPath = strPath.Substring(0, intPathLength - 25)
-
-        Dim strconnection As String = "Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;AttachDbFileName=" + strPath + "Inventory.mdf"
-        Dim dbConnection As New SqlConnection(strconnection)
-        catAdapter.Fill(ct)
-        Return ct
-    End Function
+    'Get the CategoryID of the selected item from dropdown list
+    Private Sub editingComboBox_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs)
+        Dim editingComboBox As ComboBox = TryCast(sender, ComboBox)
+        Dim dbConnection As SqlConnection = ConnectToDb()
+        dbConnection.Open()
+        Dim selectStatement As String = "SELECT CategoryID from Category WHERE Description = '" + editingComboBox.Text + "'"
+        Dim cmd As SqlCommand = New SqlCommand(selectStatement, dbConnection)
+        Dim id As Integer = cmd.ExecuteScalar()
+        MsgBox(id.ToString())
+        dbConnection.Close()
+    End Sub
 
     Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
         LoadTableData(txtSearch.Text)
     End Sub
 
-
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
-
         Try
             Dim cmd As SqlCommandBuilder = New SqlCommandBuilder(dataAdapter)
             Dim changes As DataSet = ds.GetChanges()
@@ -108,20 +120,6 @@ Public Class frmItems
     Private Sub btnNavExport_Click(sender As Object, e As EventArgs) Handles btnNavExport.Click
         Me.Hide()
         frmExport.Show()
-    End Sub
-
-    'Events for the change in dropdownlist
-    Private Sub dgvItems_CellContentClick(ByVal sender As System.Object, ByVal e As DataGridViewEditingControlShowingEventArgs) Handles dgvItems.EditingControlShowing
-        Dim editingComboBox As ComboBox = e.Control
-        AddHandler editingComboBox.SelectedIndexChanged, AddressOf editingComboBox_SelectedIndexChanged
-
-    End Sub
-
-    'Get the value from drop downlist
-    Private Sub editingComboBox_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs)
-        Dim editingComboBox As ComboBox = TryCast(sender, ComboBox)
-        varRowtext = editingComboBox.Text
-        MsgBox(varRowtext)
     End Sub
 
 End Class
